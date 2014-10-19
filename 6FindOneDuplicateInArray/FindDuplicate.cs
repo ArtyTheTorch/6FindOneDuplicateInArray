96a9183d83f134073f02fc86c84197e551b292de
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:35:06 -0700

    Finds the duplicate integers in a array
    Input: of a string that will be converted into an int array
    Returns: a list of duplicate values
    UI - Interface

---
+++ 6FindOneDuplicateInArray/FindDuplicate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _6FindOneDuplicateInArray
+{
+    class FindDuplicate
+    {
+        string[] str;
+        HashSet<string> hs = new HashSet<string>();
+        string duplicate = "";
+
+        public FindDuplicate(string p)
+        {
+            str = p.Split(',');
+            foreach(string s in str){
+                if (!hs.Add(s))
+                {
+                    duplicate += s + ", ";
+                }
+            }
+        }
+
+        public string getDuplicates()
+        {
+            return duplicate;
+        }
+    }
+}
---
+++ 6FindOneDuplicateInArray/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _6FindOneDuplicateInArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FindDuplicate x = new FindDuplicate(textBox1.Text);
+            label2.Text = x.getDuplicates();
+        }
+    }
+}
